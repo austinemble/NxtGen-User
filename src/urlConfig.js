80b691158f3085480abf61399f27154ab32df139
Austin Emmanuval Noble <austinemmanuval@gmail.com>
2021-10-20 22:58:30 +0530

    update Backend url

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://flipkart-rest-server.herokuapp.com";
+const baseUrl = "https://nxt-gen-backend.herokuapp.com/";
 
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
